18f782b5012fd7d0e44990f509173cdc970ec196
ZekarioRPL <sefdannynur12@gmail.com>
2022-03-29 09:15:59 +0700

    membuat css dan js dari dashboard

--- resources/views/Dashboard/DashboardMain.blade.php
+++ resources/views/Dashboard/DashboardMain.blade.php
@@ -1,6 +1,3 @@
-@extends('layouts.main')
-@section('container')
-    
 <!doctype html>
 <html lang="en">
   <head>
@@ -16,6 +13,7 @@
     
 
     <!-- Bootstrap core CSS -->
+
 <link href="/../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
@@ -36,7 +34,8 @@
 
     
     <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
+    <link rel="stylesheet" href="/css/DashboardMain.css">
+    <link rel="stylesheet" href="/css/dashboard.rtl.css">
   </head>
   <body>
     
@@ -281,11 +280,9 @@
 </div>
 
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/../assets/dist/js/bootstrap.bundle.min.js"></script>
 
-      <script src="/https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+      <script src="/https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
+      <script src="/js/DashboardMain.js"></script>
   </body>
 </html>
-
-
-@endsection
